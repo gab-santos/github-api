7a5e5ea94420318d0a43e5126527df9abfb5de42
Gabriel Santos <gab_santos@outlook.com>
2020-10-31 13:28:30 -0300

    chore: finish boilerplate
    add eslint, prettier and editorconfig to force code standards.
    add commitlint, commitizen and husky to check commit messages.
    delete unnecessary files.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+import { render } from "@testing-library/react";
+
+import App from "./App";
+
+test("renders learn react link", () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Hello World/i);
   expect(linkElement).toBeInTheDocument();
 });
